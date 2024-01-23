92549f107413bbcf59be18420fff4d9cbeb89fcb
Paulius Tvaranavicius - BGS <pautva@bgs.ac.uk>
2024-01-23 12:58:22 +0000

    added gitlab pages ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://kwvmxgit.ad.nerc.ac.uk/design/analysis-and-design-portfolio',
+  base: '/analysis-and-design-portfolio',
+  outDir: 'public',
+  publicDir: 'static',
+});
