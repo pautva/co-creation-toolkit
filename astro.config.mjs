7c85872352e4ea7176f357e7035306ffc4046d6a
Paulius Tvaranavicius - BGS <pautva@bgs.ac.uk>
2024-01-23 14:50:07 +0000

    config fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "http://design.glpages.ad.nerc.ac.uk/analysis-and-design-portfolio",
-  base: "/analysis-and-design-portfolio",
+  site: "http://design.glpages.ad.nerc.ac.uk",
+  base: "/analysis-and-design-portfolio/",
   outDir: "public",
   publicDir: "static",
 });
