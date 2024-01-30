9c312f13ce217c0d0874c0781ee1b51aa3cd5994
Paulius Tvaranavicius - BGS <pautva@bgs.ac.uk>
2024-01-30 10:57:34 +0000

    updated base url@

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pautva.gitlab.io',
-  base: '/analysis-and-design-portfolio',
+  base: '/analysis-and-design-portfolio/',
   outDir: 'public',
   publicDir: 'static',
   integrations: [tailwind()]
