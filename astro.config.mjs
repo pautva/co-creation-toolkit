712ef0960ae31144ce6675137d963fdc96466c97
Paulius Tvaranavicius - BGS <pautva@bgs.ac.uk>
2024-06-12 18:24:11 +0100

    remove base option

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://pautva.github.io',
-  base: '/Co-Creation-Toolkit',
-  // Change outDir to 'dist'
-  outDir: 'dist', // Output directory
-  integrations: [tailwind()]
+    site: 'https://pautva.github.io',
+    // Remove the base option
+    outDir: 'dist', // Output directory
+    integrations: [tailwind()]
 });
