a45f9dca5c25ca3fbe88ea84203e6a79812544c2
Paulius Tvaranavicius - BGS <pautva@bgs.ac.uk>
2024-06-12 18:17:25 +0100

    more fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://pautva.github.io',
   base: '/Co-Creation-Toolkit',
-  outDir: 'public',
-  publicDir: 'static',
+  // Change outDir to 'dist'
+  outDir: 'dist', // Output directory
   integrations: [tailwind()]
 });
